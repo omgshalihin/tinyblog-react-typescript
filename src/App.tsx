21bbb6c648419bff6d4861fc8fc150bf61690516
omgshalihin <shalihinsgp@gmail.com>
2023-01-16 12:36:50 +0100

    seperated articles into respective sections

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,20 @@ const mockdata = [
     tag: "crime",
   },
   {
+    title: "Top 10 places to visit in Norway this summer2",
+    image:
+      "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "August 18, 2022",
+    tag: "crime",
+  },
+  {
+    title: "Top 10 places to visit in Norway this summer3",
+    image:
+      "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "August 18, 2022",
+    tag: "crime",
+  },
+  {
     title: "Best forests to visit in North America",
     image:
       "https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
@@ -20,6 +34,20 @@ const mockdata = [
   },
   {
     title: "Hawaii beaches review: better than you think",
+    image:
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "September 9, 2022",
+    tag: "comedy",
+  },
+  {
+    title: "Hawaii beaches review: better than you think2",
+    image:
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "September 9, 2022",
+    tag: "comedy",
+  },
+  {
+    title: "Hawaii beaches review: better than you think3",
     image:
       "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
     date: "September 9, 2022",
@@ -35,21 +63,28 @@ const mockdata = [
 ];
 
 const App: FC = () => {
+  const crimeData = mockdata.filter((data) => data.tag === "crime");
+  const historyData = mockdata.filter((data) => data.tag === "history");
+  const comedyData = mockdata.filter((data) => data.tag === "comedy");
+  const mysteryData = mockdata.filter((data) => data.tag === "mystery");
   return (
     <div>
       <h1>My Tiny Blog</h1>
       <section>
         <h2>Crime</h2>
-        <ArticleList mockdata={mockdata} />
+        <ArticleList articles={crimeData} />
       </section>
       <section>
         <h2>history</h2>
+        <ArticleList articles={historyData} />
       </section>
       <section>
         <h2>comedy</h2>
+        <ArticleList articles={comedyData} />
       </section>
       <section>
         <h2>mystery</h2>
+        <ArticleList articles={mysteryData} />
       </section>
     </div>
   );
--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -13,14 +13,14 @@ import { iArticle } from "../iArticle";
 import SingleArticle from "./SingleArticle";
 
 interface iProps {
-  mockdata: iArticle[];
+  articles: iArticle[];
 }
 
-const ArticleList: FC<iProps> = ({ mockdata }) => {
+const ArticleList: FC<iProps> = ({ articles }) => {
   return (
     <Container py="xl">
       <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
-        {mockdata.map((article) => (
+        {articles.map((article) => (
           <SingleArticle key={article.title} article={article} />
         ))}
       </SimpleGrid>
